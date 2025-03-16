3d33ca641f209a05d3e0da5d58f2a5cbde141547
Micah <micah@asuh.com>
2025-03-16 18:25:20 -0400

    Work on showing spices contained in the blend
    1. Connected global state for blends and spices
    2. Using effects to grab API data for blends to get spice IDs
    3. Effect for getting spice details, but doesn't yet work
    4. Loading and Error states

--- src/frontend/blend-detail/index.js
+++ src/frontend/blend-detail/index.js
@@ -1,8 +1,62 @@
 // get blend API is at /api/v1/blends/:id
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useGlobalState } from '../../GlobalState';
+
 const BlendDetail = () => {
+  const { id } = useParams();
+  const { spices: globalSpices, blends: globalBlends } = useGlobalState();
+  const [blend, setBlend] = useState(null);
+  const [resolvedSpices, setResolvedSpices] = useState([]);
+  const [error, setError] = useState(null);
+
+  /* load blends when the page loads */
+  useEffect(() => {
+    const blendFromState = globalBlends.find((blend) => blend.id === Number.parseInt(id));
+    if (blendFromState) {
+      setBlend(blendFromState);
+    } else {
+      axios.get(`/api/v1/blends/${id}`).then((response) => {
+        setBlend(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+    }
+  }, [id, globalBlends]);
+
+  /* load spices when the page loads */
+  useEffect(() => {
+    if (blend?.spices.length > 0) {
+      const resolved = blend.spices.map((spiceId) => {
+        const spice = globalSpices.find((spice) => spice.id === spiceId);
+        console.log({spice});
+        return spice || { id: spiceId, name: `Unknown spice (ID: ${spiceId})` };
+      });
+      setResolvedSpices(resolved);
+    }
+  }, [blend, globalSpices]);
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!blend) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h2>Blend Detail Page</h2>
+      <h2>{blend.name}</h2>
+      <h3>Included Spices:</h3>
+      <ul>
+        {resolvedSpices.map((spice) => (
+          <li key={spice.id}>
+            {spice.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
